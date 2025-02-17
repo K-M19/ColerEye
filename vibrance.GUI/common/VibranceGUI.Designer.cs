4128f875e8dcc8d4786bff34613105449fe0d8ae
ButAa <113349431+K-M19@users.noreply.github.com>
2025-02-17 21:34:30 +0700

    SRC-OUT

---
+++ vibrance.GUI/common/VibranceGUI.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace vibrance.GUI.common
+{
+    partial class VibranceGUI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VibranceGUI));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.twitterToolStripTextBox = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.checkBoxAutostart = new System.Windows.Forms.CheckBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.observerStatusLabel = new System.Windows.Forms.Label();
+            this.linkLabelTwitter = new System.Windows.Forms.LinkLabel();
+            this.settingsBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonProcessExplorer = new System.Windows.Forms.Button();
+            this.buttonRemoveProgram = new System.Windows.Forms.Button();
+            this.listApplications = new System.Windows.Forms.ListView();
+            this.buttonAddProgram = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Running minimized... Like the program? Consider donating!";
+            this.notifyIcon.BalloonTipTitle = "ColerEye";
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "ColerEye";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.twitterToolStripTextBox,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(113, 48);
+            this.contextMenuStrip.Text = "ColerEye Control";
+            // 
+            // twitterToolStripTextBox
+            // 
+            this.twitterToolStripTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))));
+            this.twitterToolStripTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(69)))), ((int)(((byte)(173)))));
+            this.twitterToolStripTextBox.Image = ((System.Drawing.Image)(resources.GetObject("twitterToolStripTextBox.Image")));
+            this.twitterToolStripTextBox.Name = "twitterToolStripTextBox";
+            this.twitterToolStripTextBox.Size = new System.Drawing.Size(112, 22);
+            this.twitterToolStripTextBox.Text = "GitHub";
+            this.twitterToolStripTextBox.Click += new System.EventHandler(this.twitterToolStripTextBox_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            // 
+            // checkBoxAutostart
+            // 
+            this.checkBoxAutostart.AutoSize = true;
+            this.checkBoxAutostart.Location = new System.Drawing.Point(15, 245);
+            this.checkBoxAutostart.Name = "checkBoxAutostart";
+            this.checkBoxAutostart.Size = new System.Drawing.Size(113, 17);
+            this.checkBoxAutostart.TabIndex = 8;
+            this.checkBoxAutostart.Text = "Autostart ColerEye";
+            this.checkBoxAutostart.UseVisualStyleBackColor = true;
+            this.checkBoxAutostart.CheckedChanged += new System.EventHandler(this.checkBoxAutostart_CheckedChanged);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(50, 265);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(61, 13);
+            this.statusLabel.TabIndex = 14;
+            this.statusLabel.Text = "Initializing...";
+            this.statusLabel.Click += new System.EventHandler(this.statusLabel_Click);
+            // 
+            // observerStatusLabel
+            // 
+            this.observerStatusLabel.AutoSize = true;
+            this.observerStatusLabel.Location = new System.Drawing.Point(12, 265);
+            this.observerStatusLabel.Name = "observerStatusLabel";
+            this.observerStatusLabel.Size = new System.Drawing.Size(43, 13);
+            this.observerStatusLabel.TabIndex = 13;
+            this.observerStatusLabel.Text = "Status: ";
+            // 
+            // linkLabelTwitter
+            // 
+            this.linkLabelTwitter.AutoSize = true;
+            this.linkLabelTwitter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelTwitter.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabelTwitter.Location = new System.Drawing.Point(324, 245);
+            this.linkLabelTwitter.Name = "linkLabelTwitter";
+            this.linkLabelTwitter.Size = new System.Drawing.Size(83, 24);
+            this.linkLabelTwitter.TabIndex = 10;
+            this.linkLabelTwitter.TabStop = true;
+            this.linkLabelTwitter.Text = "@K-M19";
+            this.linkLabelTwitter.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelTwitter_LinkClicked);
+            // 
+            // settingsBackgroundWorker
+            // 
+            this.settingsBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.settingsBackgroundWorker_DoWork);
+            this.settingsBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.settingsBackgroundWorker_RunWorkerCompleted);
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutoPopDelay = 5000;
+            this.toolTip.InitialDelay = 100;
+            this.toolTip.IsBalloon = true;
+            this.toolTip.ReshowDelay = 100;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Controls.Add(this.buttonProcessExplorer);
+            this.groupBox5.Controls.Add(this.buttonRemoveProgram);
+            this.groupBox5.Controls.Add(this.listApplications);
+            this.groupBox5.Controls.Add(this.buttonAddProgram);
+            this.groupBox5.Location = new System.Drawing.Point(10, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(397, 227);
+            this.groupBox5.TabIndex = 18;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Settings";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonProcessExplorer
+            // 
+            this.buttonProcessExplorer.Location = new System.Drawing.Point(7, 20);
+            this.buttonProcessExplorer.Name = "buttonProcessExplorer";
+            this.buttonProcessExplorer.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcessExplorer.TabIndex = 3;
+            this.buttonProcessExplorer.Text = "Add";
+            this.buttonProcessExplorer.UseVisualStyleBackColor = true;
+            this.buttonProcessExplorer.Click += new System.EventHandler(this.buttonProcessExplorer_Click);
+            // 
+            // buttonRemoveProgram
+            // 
+            this.buttonRemoveProgram.Location = new System.Drawing.Point(186, 20);
+            this.buttonRemoveProgram.Name = "buttonRemoveProgram";
+            this.buttonRemoveProgram.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemoveProgram.TabIndex = 2;
+            this.buttonRemoveProgram.Text = "Remove";
+            this.buttonRemoveProgram.UseVisualStyleBackColor = true;
+            this.buttonRemoveProgram.Click += new System.EventHandler(this.buttonRemoveProgram_Click);
+            // 
+            // listApplications
+            // 
+            this.listApplications.HideSelection = false;
+            this.listApplications.Location = new System.Drawing.Point(7, 49);
+            this.listApplications.Name = "listApplications";
+            this.listApplications.Size = new System.Drawing.Size(384, 172);
+            this.listApplications.TabIndex = 1;
+            this.listApplications.UseCompatibleStateImageBehavior = false;
+            this.listApplications.DoubleClick += new System.EventHandler(this.listApplications_DoubleClick);
+            // 
+            // buttonAddProgram
+            // 
+            this.buttonAddProgram.Location = new System.Drawing.Point(88, 20);
+            this.buttonAddProgram.Name = "buttonAddProgram";
+            this.buttonAddProgram.Size = new System.Drawing.Size(92, 23);
+            this.buttonAddProgram.TabIndex = 0;
+            this.buttonAddProgram.Text = "Add manually";
+            this.buttonAddProgram.UseVisualStyleBackColor = true;
+            this.buttonAddProgram.Click += new System.EventHandler(this.buttonAddProgram_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Version: 0.1";
+            // 
+            // VibranceGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 288);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.checkBoxAutostart);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.observerStatusLabel);
+            this.Controls.Add(this.linkLabelTwitter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "VibranceGUI";
+            this.Text = "ColerEye";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem twitterToolStripTextBox;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.CheckBox checkBoxAutostart;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label observerStatusLabel;
+        private System.Windows.Forms.LinkLabel linkLabelTwitter;
+        private System.ComponentModel.BackgroundWorker settingsBackgroundWorker;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button buttonRemoveProgram;
+        private System.Windows.Forms.ListView listApplications;
+        private System.Windows.Forms.Button buttonAddProgram;
+        private System.Windows.Forms.Button buttonProcessExplorer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
